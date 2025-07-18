df7fc24810ed8b0cad7eec09b4f5949d7d9a7ac7
jason-vila <jasonvilac@gmail.com>
2025-07-17 23:25:34 -0500

    🔧 chore(config): add aliases for Astro components, layouts, views, and pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-import path from 'node:path';
+import { defineConfig } from "astro/config";
+import path from "node:path";
 
 export default defineConfig({
   alias: {
-    '@': path.resolve('./src'),
+    "@": path.resolve("./src"),
+    "@components": path.resolve("./src/components"),
+    "@layouts": path.resolve("./src/layouts"),
+    "@views": path.resolve("./src/views"),
+    "@pages": path.resolve("./src/pages"),
   },
 });

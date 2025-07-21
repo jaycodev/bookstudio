77e39196e2eb4e1c1ed9b31d1acd2ee03b45f377
jason-vila <jasonvilac@gmail.com>
2025-07-21 15:34:29 -0500

    🎨 style(ui): apply dynamic close button color in toast based on alert type

--- public/js/shared/utils/ui/toast.js
+++ public/js/shared/utils/ui/toast.js
@@ -17,18 +17,18 @@ export function showToast(message, type) {
       borderClasses =
         'text-success-emphasis bg-success-subtle border border-success-subtle rounded-2'
       iconClass = 'bi-check2-circle'
-      btnCloseClass = 'btn-close-success'
+      btnCloseClass = 'text-success-emphasis'
       break
     case 'error':
       borderClasses = 'text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-2'
       iconClass = 'bi-x-circle'
-      btnCloseClass = 'btn-close-danger'
+      btnCloseClass = 'text-danger-emphasis'
       break
     case 'warning':
       borderClasses =
         'text-warning-emphasis bg-warning-subtle border border-warning-subtle rounded-2'
       iconClass = 'bi-exclamation-triangle'
-      btnCloseClass = 'btn-close-warning'
+      btnCloseClass = 'text-warning-emphasis'
       break
   }
 
@@ -38,11 +38,13 @@ export function showToast(message, type) {
   toastElement.setAttribute('aria-live', 'assertive')
   toastElement.setAttribute('aria-atomic', 'true')
   toastElement.innerHTML = `
-    <div class="d-flex">
+    <div class="d-flex px-1">
       <div class="toast-body d-flex align-items-center">
         <i class="bi ${iconClass} me-2"></i>${message}
       </div>
-      <button type="button" class="btn-close ${btnCloseClass} me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+      <button type="button" class="btn m-0 p-0 border-0 ${btnCloseClass} ms-auto me-2" data-bs-dismiss="toast" aria-label="Close">
+        <i class="bi bi-x-lg fs-6"></i>
+      </button>
     </div>
   `
 

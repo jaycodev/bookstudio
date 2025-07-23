305edf91c852b838ee921368e63d912ecde416b0
jason-vila <jasonvilac@gmail.com>
2025-07-23 12:32:58 -0500

    🎨 style(ui): set theme and meta color early for consistent dark/light mode in index.html

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,9 +1,18 @@
 import { Moon, Sun } from "lucide-react";
+import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
+
+  useEffect(() => {
+    const themeColor =
+      theme === "dark" ? "oklch(0.141 0.005 285.823)" : "oklch(1 0 0)";
+
+    const metaThemeColor = document.querySelector("meta[name='theme-color']");
+    if (metaThemeColor) metaThemeColor.setAttribute("content", themeColor);
+  }, [theme]);
 
   return (
     <Button

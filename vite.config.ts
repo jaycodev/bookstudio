c3977b9985d486415191ea896d042132ad584d55
jason-vila <jasonvilac@gmail.com>
2025-07-24 13:22:18 -0500

    🔧 chore(deps): update project dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
   esbuild: false,
   resolve: {
     alias: {

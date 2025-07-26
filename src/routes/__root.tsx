bf0dafc69b68ee093dd7357d2212446f5b1df7e0
jason-vila <jasonvilac@gmail.com>
2025-07-26 16:19:28 -0500

    ♻️ refactor(lib): move document-title.tsx to /lib

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,9 +2,9 @@ import { QueryClient } from '@tanstack/react-query'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 
 import NavigationProgress from '@/components/feedback/navigation-progress'
-import DocumentTitle from '@/components/shared/utils/document-title'
 import GeneralError from '@/features/errors/pages/general'
 import NotFoundError from '@/features/errors/pages/not-found'
+import DocumentTitle from '@/lib/document-title'
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient

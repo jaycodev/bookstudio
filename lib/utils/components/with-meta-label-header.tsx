20167c539022096f076fb2e87a5cd0d254f7844a
jaycodev <jason.vilac@gmail.com>
2025-12-03 10:28:33 -0500

    chore(whitespace): remove unnecessary blank line in with-meta-label-header component

--- lib/utils/components/with-meta-label-header.tsx
+++ lib/utils/components/with-meta-label-header.tsx
@@ -3,7 +3,6 @@
 import type { HeaderContext } from '@tanstack/react-table'
 
 import { DataTableColumnHeader } from '@admin/components/data-table/data-table-column-header'
-
 import { getColumnLabel } from '@admin/config/column-labels'
 
 export function withMetaLabelHeader<TData, TValue = unknown>() {
